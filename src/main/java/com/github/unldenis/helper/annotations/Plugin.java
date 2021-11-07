bcd7c7a846370f27e28927e3bbf8f0937219d9f0
unldenis <mehillideny13@gmail.com>
2021-11-07 17:06:29 +0100

    add

--- src/main/java/com/github/unldenis/helper/annotations/Plugin.java
+++ src/main/java/com/github/unldenis/helper/annotations/Plugin.java
@@ -5,9 +5,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Plugin {
+
     String name();
+
     String version();
+
+    String description();
+
+    String api_version() default "1.13";
+
+    String author() default "unldenis";
+
+    String[] depend() default "";
+
+    String[] softdepend() default "";
+
+    String[] loadbefore() default "";
+
+    String[] commands() default "";
+
 }
